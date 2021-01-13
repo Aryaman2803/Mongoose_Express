3055125f6c5037a7a44282127030a08ff6669d23
aryaman2803 <aryamansingh2803@gmail.com>
2021-01-13 23:23:17 +0530

    master

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const port = 3000;
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const AppError = require("./AppError");
 
 const Product = require("./models/product");
 mongoose
@@ -53,30 +54,51 @@ app.get("/products/new", (req, res) => {
 
 //(5) That new Form submission is routed here
 //(5.1) WE TELL EXPRESS TO USE THAT MIDDLEWARE  (WRITTEN ON TOP)
-app.post("/products", async (req, res) => {
+app.post("/products", wrapAsync(async (req, res, next) => {
   // console.log(req.body);
   //(5.2) Process to add newly entered product in Database
-  const newProduct = new Product(req.body);
-  await newProduct.save();
-  console.log(newProduct);
-  res.redirect("/products");
-});
+  
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    console.log(newProduct);
+    res.redirect("/products");
+ 
+}));
+
+function wrapAsync(fn) {
+  return function (req, res, next) {
+    fn(req, res, next).catch((e) => next(e));
+  };
+}
 
 //(3)Get product details route
-app.get("/products/:id", async (req, res) => {
-  //(3.1) We grab/find an ID in req.params
-  const { id } = req.params;
-  //(3.2) Find the ID in Products Model and make it await for mongoose operation
-  const product = await Product.findById(id);
-  res.render("products/show", { product });
-});
+app.get("/products/:id",  wrapAsync(async (req, res, next) => {
+    //(3.1) We grab/find an ID in req.params
+
+    const { id } = req.params;
+    //(3.2) Find the ID in Products Model and make it await for mongoose operation
+    const product = await Product.findById(id);
+    if (!product) {
+      throw next(new AppError("Item not found in the database", 404));
+    }
+    res.render("products/show", { product });
+  })
+);
 
 //(6) Route For Edit Form
-app.get("/products/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  res.render("products/edit", { product, categories });
-});
+app.get(
+  "/products/:id/edit",
+  wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      return next(
+        new AppError("Item not found in the database so no edit", 404)
+      );
+    }
+    res.render("products/edit", { product, categories });
+  })
+);
 
 //(7) We are updating form so we use PUT/PATCH any is fine
 /*(7.1) NOW the problem is that we can't actually make a put request,
@@ -87,22 +109,43 @@ app.get("/products/:id/edit", async (req, res) => {
  
 */
 
-app.put("/products/:id/", async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findByIdAndUpdate(id, req.body, {
-    runValidators: true,
-    new: true,
-  });
-  console.log(req.body);
-  res.redirect(`/products/${product._id}`);
-});
+app.put("/products/:id/",wrapAsync (async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+      runValidators: true,
+      new: true,
+    });
+    console.log(req.body);
+    res.redirect(`/products/${product._id}`);
+  } catch (e) {
+    next(e);
+  }
+}));
 
 //(8)Make an DELETE Request. we can't actually do that in HTML Form in the browser but we can fake it
 // We can send a POST request then add on the method override query string
 app.delete("/products/:id", async (req, res) => {
   const { id } = req.params;
   const deleteProduct = await Product.findByIdAndDelete(id);
-  res.redirect('/products')
+  res.redirect("/products");
+});
+
+const handleValidationError = err=>{
+  console.log(err)
+  return err
+}
+app.use((err,req,res,next)=>{
+  console.log(err.name)
+  if(err.name === 'ValidationError') err= handleValidationError(err)
+  next(err)
+})
+
+//(9) Basic Error Handling middleware
+//(9.1) Firstly we destructure the err Object that was passed in this use method
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Something went wrong" } = err;
+  res.status(status).send(message);
 });
 
 app.listen(port, () => {
